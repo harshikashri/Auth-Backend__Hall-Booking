955090466c3624fcac3226d78b5290461727eb08
harshikashri <harshikashribalaraja@gmail.com>
2026-05-27 13:37:33 +0530

    updated settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,15 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str | None = None
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
-    POSTGRES_DB: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL = settings.DATABASE_URL
+
+    if DATABASE_URL is None:
+        raise ValueError("DATABASE_URL is not set")
 
     model_config = {
         "env_file": ".env"
